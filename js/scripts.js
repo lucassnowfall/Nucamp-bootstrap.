1c9721fe5f4be86f1f18e723c3ee554a8d8a0314
Lucas de Souza <lucasdesouzaofficial@gmail.com>
2020-03-28 12:07:55 -0700

    End of Week 4 Exercises :D

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,13 @@ $(function () {
         }
     });
 });
+
+$(document).ready(function () {
+    $("#reserveButton").click(function () {
+        $("#reserveModal").modal("show");
+    });
+    $("#loginButton").click(function () {
+        $("#loginModal").modal("show");
+    });
+});
+
